a89fe87bc6c2fa30181fafccd33bc9b54b27626a
harrypango <harry.pango@gmail.com>
2024-03-03 20:09:48 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <div class="min-h-full">
+      <div
+        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+      ></div>
+
+      <!-- Left sidebar -->
+      <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
+        Test
+      </div>
+      <!-- Main content -->
+      <!-- Right sidebar -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+const darkMode = ref(true);
+</script>
