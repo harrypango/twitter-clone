040d4ffbd0d718553527540c9c747f152d020d1a
harrypango <harry.pango@gmail.com>
2024-03-12 16:10:37 +0100

    backend connection

--- app.vue
+++ app.vue
@@ -1,20 +1,35 @@
 <template>
   <div :class="{ dark: darkMode }">
-    <div class="min-h-full">
-      <div
-        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
-      ></div>
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <!-- Left sidebar -->
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0"></div>
+            <SidebarLeft />
+          </div>
 
-      <!-- Left sidebar -->
-      <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
-        Test
+          <!-- Main content -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view />
+          </main>
+
+          <!-- Right sidebar -->
+          <div class="hidden col-span-12 md:block md:col-span-3 xl:col-span-4">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
+          </div>
+        </div>
       </div>
-      <!-- Main content -->
-      <!-- Right sidebar -->
     </div>
   </div>
 </template>
 
 <script setup>
-const darkMode = ref(true);
+import index from "./components/Sidebar/Left/index.vue";
+
+const darkMode = ref(false);
 </script>
