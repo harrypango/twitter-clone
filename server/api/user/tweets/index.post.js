6b1cfd33e418c28486933fa977027335cd96782a
harrypango <harry.pango@gmail.com>
2024-03-23 02:27:23 +0100

    critical bug fixes

--- server/api/user/tweets/index.post.js
+++ server/api/user/tweets/index.post.js
@@ -1,4 +1,8 @@
 import formidable from "formidable";
+import { tweetTransformer } from "~~/server/transformers/tweet.js";
+import { createTweet } from "../../../db/tweets.js";
+import { createMediaFile } from "../../../db/mediaFiles.js";
+import { uploadToCloudinary } from "../../../utils/cloudinary.js";
 
 export default defineEventHandler(async (event) => {
   const form = formidable({});
@@ -9,11 +13,44 @@ export default defineEventHandler(async (event) => {
         reject(err);
       }
       resolve({ fields, files });
-      console.log(files.image[0].filepath);
     });
   });
 
+  const { fields, files } = response;
+
+  const userId = event.context?.auth?.user?.id;
+
+  const tweetData = {
+    text: String(fields.text),
+    authorId: userId,
+  };
+
+  // const replyTo = fields.replyTo;
+
+  // if (replyTo && replyTo !== "null" && replyTo !== "undefined") {
+  //   tweetData.replyToId = replyTo;
+  // }
+
+  const tweet = await createTweet(tweetData);
+  const filePromises = Object.keys(files.image).map(async (key) => {
+    const file = files.image[key];
+
+    const response = await uploadToCloudinary(file.filepath);
+
+    console.log(response);
+
+    return createMediaFile({
+      url: "",
+      providerPublicId: "random_id",
+      userId: userId,
+      tweetId: tweet.id,
+    });
+  });
+
+  await Promise.all(filePromises);
+
   return {
-    hello: response,
+    // tweet: tweetTransformer(tweet),
+    files,
   };
 });
